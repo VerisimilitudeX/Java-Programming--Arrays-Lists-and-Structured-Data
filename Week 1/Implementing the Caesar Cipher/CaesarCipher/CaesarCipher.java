840f267ef1a12f59f9ffe95ccb5b74724a12d00b
Piyush Acharya <acharyapiyush1@outlook.com>
2022-08-18 00:12:27 -0700

    Changed to use ASCII method
    More accurate

--- Week 1/Implementing the Caesar Cipher/CaesarCipher/CaesarCipher.java
+++ Week 1/Implementing the Caesar Cipher/CaesarCipher/CaesarCipher.java
@@ -1,6 +1,29 @@
 public class CaesarCipher {
     public static String encrypt(String input, int key) {
-        String[] alphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
-        
+        char[] charInput = input.toCharArray();
+        String encrypted = "";
+        for (char ch : charInput) {
+            int ASCII = (int) ch;
+            ASCII += key;
+            if (!Character.isAlphabetic(ch)) {
+                ;
+                encrypted += Character.toString(ch);
+                continue;
+            } else if (ASCII > 90) {
+                ASCII -= 90;
+                if (Character.isUpperCase(ch)) {
+                    ASCII += 64;
+                } else if (Character.isLowerCase(ch)) {
+                    ASCII += 96;
+                }
+            }
+            ch = (char) ASCII;
+            encrypted += Character.toString(ch);
+        }
+        return encrypted;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(encrypt("FIRST LEGION ATTACK EAST FLANK!", 23));
     }
 }
