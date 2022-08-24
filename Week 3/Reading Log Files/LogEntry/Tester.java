466c548acf8254c6d75d5e6f405ee693184a2529
Piyush Acharya <acharyapiyush1@outlook.com>
2022-08-24 13:39:26 -0700

    Added files for programming exercise part1 in week 3

---
+++ Week 3/Reading Log Files/LogEntry/Tester.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Tester {
+    public void testLogEntry() {
+        LogEntry le = new LogEntry("1.2.3.4", new Date(), "example request", 200, 500);
+        System.out.println(le);
+        LogEntry le2 = new LogEntry("1.2.100.4", new Date(), "example request 2", 300, 400);
+        System.out.println(le2);
+    }
+
+    public void testLogAnalyzer() {
+        // complete method
+    }
+}
---
+++ Week 3/Reading Log Files/WebLogProgram/LogAnalyzer.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class LogAnalyzer here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import java.util.*;
+
+public class LogAnalyzer {
+    private ArrayList<LogEntry> records;
+
+    public LogAnalyzer() {
+        // complete constructor
+    }
+
+    public void readFile(String filename) {
+        // complete method
+    }
+
+    public void printAll() {
+        for (LogEntry le : records) {
+            System.out.println(le);
+        }
+    }
+
+}
